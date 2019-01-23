6b8b52e6aa9d380ec3b4a4992f829cc53a8c6679
Drake Rentrop <drakerentrop@gmail.com>
2019-01-23 14:39:13 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,15 @@
+class Song
+  attr_accessor :name, :artist, :genre
+  @@count = 0
+  @@artists = []
+  @@genres = []
+
+  def initialize(song_name, artists, genre)
+    @name = song_name
+    @artist = artist
+    @genre = genre
+    @count += 1
+    @@artist << artist
+    @@genre << genre
+  end
+  
